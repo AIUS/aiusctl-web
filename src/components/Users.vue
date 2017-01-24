60369db591a8d5ba314bc953778c9d6a75460e39
ludovicm67 <ludovicmuller1@gmail.com>
2017-01-24 15:56:40 +0100

    Template for Users

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="users">
-    <h1> Users </h1>
+    <h1>Gestion des utilisateurs</h1>
+    <md-table v-once>
+      <md-table-header>
+        <md-table-head>ID</md-table-head>
+        <md-table-head>Nom</md-table-head>
+        <md-table-head>Prénom</md-table-head>
+        <md-table-head>Adresse mail</md-table-head>
+        <md-table-head>Actions</md-table-head>
+      </md-table-header>
+
+      <md-table-body>
+        <md-table-row v-for="(row, index) in 42" :key="index">
+          <md-table-cell>{{ row }}</md-table-cell>
+          <md-table-cell>Doe</md-table-cell>
+          <md-table-cell>John</md-table-cell>
+          <md-table-cell>john.doe@example.com</md-table-cell>
+          <md-table-cell><a>Editer</a> - <a>Supprimer</a></md-table-cell>
+        </md-table-row>
+      </md-table-body>
   </div>
 </template>
 
