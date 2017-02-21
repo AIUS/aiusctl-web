ed9569b1e76dad97eaf6f9ee6a112daf78c06dea
ludovicm67 <ludovicmuller1@gmail.com>
2017-02-22 00:40:33 +0100

    better ui

--- src/components/pages/Sales.vue
+++ src/components/pages/Sales.vue
@@ -4,22 +4,12 @@
 
     <section v-for="category in categories">
       <h2>{{ category.name }}</h2>
-      <md-table>
-      <md-table-header>
-        <md-table-row>
-          <md-table-head>Nom du produit</md-table-head>
-          <md-table-head>Prix</md-table-head>
-          <md-table-head>Action</md-table-head>
-        </md-table-row>
-      </md-table-header>
-      <md-table-body>
-        <md-table-row v-for="product in category.products">
-          <md-table-cell>{{ product.name }}</md-table-cell>
-          <md-table-cell>{{ product.price.toFixed(2) }} €</md-table-cell>
-          <md-table-cell><md-button class="md-icon-button" @click="$store.dispatch('sales/addProduct', product.id)"><md-icon>add_shopping_cart</md-icon></md-table-cell>
-        </md-table-row>
-      </md-table-body>
-    </md-table>
+      <div class="tiles">
+        <div class="tile" v-for="product in category.products" @click="$store.dispatch('sales/addProduct', product.id)">
+          <p>{{ product.name }}</p>
+          <p>{{ product.price.toFixed(2) }} €</p>
+        </div>
+      </div>
     </section>
 
     <section v-for="category in categories">
@@ -75,5 +65,21 @@ export default {
 </script>
 
 <style scoped>
-
+.tiles {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+}
+.tile {
+  user-select: none;
+  padding: 25px;
+  margin-bottom: 12px;
+  background-color: #3f51b5;
+  color: #fff;
+  text-align: center;
+  cursor: pointer;
+}
+.tile > p:nth-child(2) {
+  font-size: 24px;
+}
 </style>
